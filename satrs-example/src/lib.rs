4a2d5396e3b8e27a38c65f6a8657c91127f71aaa
Jose Manuel Diez <j.diezlopez@tu-berlin.de>
2024-07-09 20:46:58 +0200

    Revert "Make more modules public in satrs-example"
    This reverts commit ccfeb41b315834bac0144ea05ee10e3466331fe1.

--- satrs-example/src/lib.rs
+++ satrs-example/src/lib.rs
@@ -1,10 +1,6 @@
 use satrs::spacepackets::time::{cds::CdsTime, TimeWriter};
 
 pub mod config;
-pub mod pus;
-pub mod tmtc;
-pub mod request;
-
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum DeviceMode {
